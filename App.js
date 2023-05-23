1c272b1a492de2ac9fc92b870fe73a76719d257a
evanbacon <bacon@expo.io>
2023-05-23 17:47:07 -0500

    Initial commit
    Generated by create-expo 2.0.0.

---
+++ App.js
@@ -0,0 +1,140 @@
+import { Asset } from "expo-asset";
+import Constants from "expo-constants";
+import * as SplashScreen from "expo-splash-screen";
+import * as Updates from "expo-updates";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import {
+  Animated,
+  Button,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+
+// Instruct SplashScreen not to hide yet, we want to do this manually
+SplashScreen.preventAutoHideAsync().catch(() => {
+  /* reloading the app might trigger some race conditions, ignore them */
+});
+
+export default function App() {
+  return (
+    <AnimatedAppLoader image={{ uri: Constants.manifest.splash.image }}>
+      <MainScreen />
+    </AnimatedAppLoader>
+  );
+}
+
+function AnimatedAppLoader({ children, image }) {
+  const [isSplashReady, setSplashReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      await Asset.fromURI(image.uri).downloadAsync();
+      setSplashReady(true);
+    }
+
+    prepare();
+  }, [image]);
+
+  if (!isSplashReady) {
+    return null;
+  }
+
+  return <AnimatedSplashScreen image={image}>{children}</AnimatedSplashScreen>;
+}
+
+function AnimatedSplashScreen({ children, image }) {
+  const animation = useMemo(() => new Animated.Value(1), []);
+  const [isAppReady, setAppReady] = useState(false);
+  const [isSplashAnimationComplete, setAnimationComplete] = useState(false);
+
+  useEffect(() => {
+    if (isAppReady) {
+      Animated.timing(animation, {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: true,
+      }).start(() => setAnimationComplete(true));
+    }
+  }, [isAppReady]);
+
+  const onImageLoaded = useCallback(async () => {
+    try {
+      await SplashScreen.hideAsync();
+      // Load stuff
+      await Promise.all([]);
+    } catch (e) {
+      // handle errors
+    } finally {
+      setAppReady(true);
+    }
+  }, []);
+
+  return (
+    <View style={{ flex: 1 }}>
+      {isAppReady && children}
+      {!isSplashAnimationComplete && (
+        <Animated.View
+          pointerEvents="none"
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              backgroundColor: Constants.manifest.splash.backgroundColor,
+              opacity: animation,
+            },
+          ]}
+        >
+          <Animated.Image
+            style={{
+              width: "100%",
+              height: "100%",
+              resizeMode: Constants.manifest.splash.resizeMode || "contain",
+              transform: [
+                {
+                  scale: animation,
+                },
+              ],
+            }}
+            source={image}
+            onLoadEnd={onImageLoaded}
+            fadeDuration={0}
+          />
+        </Animated.View>
+      )}
+    </View>
+  );
+}
+
+function MainScreen() {
+  const onReloadPress = useCallback(() => {
+    if (Platform.OS === "web") {
+      location.reload();
+    } else {
+      Updates.reloadAsync();
+    }
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "plum",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text
+        style={{
+          color: "black",
+          fontSize: 30,
+          marginBottom: 15,
+          fontWeight: "bold",
+        }}
+      >
+        Pretty Cool!
+      </Text>
+      <Button title="Run Again" onPress={onReloadPress} />
+    </View>
+  );
+}
